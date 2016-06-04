37bdc6af1fb1f97e300c0aae5888b4a3cda49c52
rackso55 <oscarvilla55@hotmail.com>
2016-06-04 12:12:12 -0500

    LOOOOOOOOOOOOOOOOOOOL
    lo siento por el fallo

---
+++ Animate/src/java/beans/SesionC.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import logic.SesionL;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import model.Estudiante;
+import model.Usuario;
+import model.Administrador;
+
+
+@Named(value = "sesionC")
+@SessionScoped
+@ManagedBean
+public class SesionC implements Serializable {
+
+   private Usuario es = new Usuario();
+   private boolean tipo;
+   private FacesMessage mensaje;
+   private Administrador a = new Administrador();
+
+   
+    public Usuario getUsuario() {
+        return es;
+    }
+
+    public void setUsuario(Usuario es) {
+        this.es = es;
+    }
+
+    public String verificarDatos() throws Exception {
+        SesionL sl = new SesionL();
+        String resultado;
+        if(tipo == false) {
+            Usuario est;
+            try {
+            est = sl.verificarDatos(this.es);
+            if (est != null) {
+                FacesContext.getCurrentInstance().getExternalContext()
+                        .getSessionMap().put("usuario", est);
+                resultado = "PaginaInicioSesionIH";
+                es = est;
+            } else {
+                mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario o contraseña incorrectos", null);
+                FacesContext.getCurrentInstance().addMessage(null, mensaje);
+                resultado = "";
+            }
+           
+            } catch (Exception e) {
+            throw e;
+            }
+        } else {
+            resultado = "";
+        }/* else {    
+            Administrador adm;
+            try {
+            adm = sl.verificarDatos(this.a);
+            if (adm != null) {
+                FacesContext.getCurrentInstance().getExternalContext()
+                        .getSessionMap().put("usuario", adm);
+                resultado = "inicioAdmin";
+                a = adm;
+            } else {
+                mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario o contraseña incorrectos", null);
+                FacesContext.getCurrentInstance().addMessage(null, mensaje);
+                resultado = "";
+            }
+            } catch (Exception e) {
+            throw e;
+            }
+        }*/
+        
+        return resultado;
+    }
+
+    public boolean verificarSesion() {
+        boolean estado;
+        if (FacesContext.getCurrentInstance().getExternalContext()
+                .getSessionMap().get("usuario") == null) {
+            estado = false;
+        } else {
+            estado = true;
+        }
+
+        return estado;
+    }
+
+    public String cerrarSesion() {
+        FacesContext.getCurrentInstance().getExternalContext()
+                .invalidateSession();
+        /* is all of this necessary?*/
+        es = null;
+        a = null;
+        return "index.xhtml";
+    }    
+    
+    public boolean verificarTipo() {
+        String tipo = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario").getClass().getSimpleName();
+        if(tipo.equals("Administrador"))
+            return true;
+        return false;
+    }
+
+        
+}
