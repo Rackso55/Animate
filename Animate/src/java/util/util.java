154228cbbb82650d499a4abcf20dfbd048267d93
andreagonz <andreagonz@ciencias.unam.mx>
2016-06-03 22:38:06 -0500

    Caroeta de beans y archivo de la conexion

--- Animate/src/java/util/util.java
+++ Animate/src/java/util/util.java
@@ -23,7 +23,7 @@ public class util {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            sessionFactory = new AnnotationConfiguration().configure("xml/hibernate.cfg.xml").buildSessionFactory();
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
